af39dfc791ee546b54a135097677fc17464c27d4
Rahul Jadhav <jadhav.rj.cmos@gmail.com>
2023-08-30 22:15:50 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,21 +8,50 @@ function Home() {
   const [currentTime, setCurrentTime] = useState(0);
   const [laps, setLaps] = useState([]);
 
+  const startHandler = () => {
+    if (intervalRef.current === 0) {
+      startTime.current = Date.now() - currentTime;
+      intervalRef.current = setInterval(() => {
+        setCurrentTime(Date.now() - startTime.current);
+      }, 10);
+    }
+  };
+
+  const stopHandler = () => {
+    if (intervalRef.current !== 0) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = 0;
+    }
+  };
+
+  const lapHandler = () => {
+    setLaps(prevLaps => [...prevLaps, currentTime]);
+  };
+
+  const resetHandler = () => {
+    stopHandler();
+    setCurrentTime(0);
+    setLaps([]);
+  };
+
   return (
     <div id="main">
       <section>
         <h1 className='seconds-elapsed'>Stopwatch Time</h1>
         <section className='buttons'>
-          <button className="start-btn">START</button>
-          <button className="stop-btn">STOP</button>
-          <button className="lap-btn">LAP</button>
-          <button className="reset-btn">RESET</button>
+          <button className="start-btn" onClick={startHandler}>START</button>
+          <button className="stop-btn" onClick={stopHandler}>STOP</button>
+          <button className="lap-btn" onClick={lapHandler}>LAP</button>
+          <button className="reset-btn" onClick={resetHandler}>RESET</button>
         </section>
       </section>
       <section className='lap-section'>
         <h2>Laps</h2>
         <section className='laps'>
           <p>lap</p>
+          {laps.map((lapTime, index) => (
+            <p key={index}>{(lapTime / 1000).toFixed(3)}</p>
+          ))}
           <p>lap</p>
           <p>lap</p>
         </section>
